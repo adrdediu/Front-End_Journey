c78e001fdbd7be53cb3b82c3f4f7c9b077e33afc
Adrian-Nicolae Dediu <111912660+adrdediu@users.noreply.github.com>
2023-01-08 16:20:08 +0200

    6. Added the final code for the feature. Now I shall add some tests

--- Final Certification Project - Reserve a Table feature/reserve-a-table/src/App.js
+++ Final Certification Project - Reserve a Table feature/reserve-a-table/src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/Footer"
 import ReservationDetails from"./components/ReservationDetails"
 import ReservationAccountCreation from './components/ReservationAccountCreation';
 import { Routes, Route} from "react-router-dom";
+import ReservationConfirmation from './components/ReservationConfirmation';
 //import { useLayoutEffect, useRef, useState } from 'react';
 
 function App() {
@@ -18,7 +19,8 @@ function App() {
       <Routes>
           <Route exact path="/" element={<Main/>} />
           <Route exact path="/reservations" element={<ReservationDetails/>}/>
-          <Route exact path="/reservations-create-account" element={<ReservationAccountCreation/>} />
+          <Route exact path="/reservation-create-account" element={<ReservationAccountCreation/>} />
+          <Route exact path="/reservation-confirmation" element={<ReservationConfirmation/>} />
           <Route component={Error} />
       </Routes>
       <Footer/>
--- Final Certification Project - Reserve a Table feature/reserve-a-table/src/components/ConfirmButton.js
+++ Final Certification Project - Reserve a Table feature/reserve-a-table/src/components/ConfirmButton.js
@@ -1,13 +1,11 @@
-import { Link } from "react-router-dom"
+
 import { HStack } from "@chakra-ui/react"
 export default function ConfirmButton(props) {
     return(
-        <Link to={props.href}>
-        <button className="confirm-button">
+        <button className="confirm-button" type={props.type} onClick={props.onSubmit}>
             <HStack justify="center">
                 <p>{props.title}</p>
             </HStack>
         </button>
-        </Link>
     )
 }
